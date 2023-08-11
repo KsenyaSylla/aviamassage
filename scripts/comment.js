89c3d46daa892392dc8ec23b717f101fb192f5e7
Sylla Ksenia <natoire@yandex.ru>
2023-08-11 22:01:55 +0300

    Made template for comment

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,12 +1,23 @@
 export function comment(comments, container) {
-    console.log("working");
 
-    const article = document.createElement("article");
-    article.classList.add("comments__item");
-    container.appendChild("beforeend", article); //проверить правильность места, чтобы последовательно все вставлялись по мере продвижения по массиву
+    comments.forEach(comment => {
+        const article = document.createElement("article");
+        article.classList.add("comments__item");
+        container.appendChild(article); //проверить правильность места, чтобы последовательно все вставлялись по мере продвижения по массиву
 
-    const heading = document.createElement("h3");
-    heading.classList.add("comments__item--name");
-    heading.innerText = comments.name;
-    article
+        const heading = document.createElement("h3");
+        heading.classList.add("comments__item--name");
+        heading.innerText = comment.name;
+        article.appendChild(heading);
+
+        const text = document.createElement("p");
+        text.classList.add("comments__item--text");
+        text.innerText = comment.text;
+        article.appendChild(text);
+
+        const date = document.createElement("div");
+        date.classList.add("comments__item--date");
+        date.innerText = comment.date;
+        article.appendChild(date);
+    });
 }
