8a39c74fadd67690e30c527ed9e0c3e9c3093695
Sylla Ksenia <natoire@yandex.ru>
2023-08-25 22:23:22 +0300

    contacts

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,14 +1,14 @@
 export function burger() {
     const burgerMenu = document.querySelector(".navbar__burger");
     const burgerList = document.querySelector(".navbar__list");
+
     burgerMenu.addEventListener("click", () => {
         if (burgerList.style.display == "flex") {
             burgerList.style.display = "none";
-            burgerMenu.firstElementChild.style.display = "block";
+            burgerMenu.firstElementChild.style.display = "flex";
         } else {
             burgerList.style.display = "flex";
             burgerMenu.firstElementChild.style.display = "none";
         }
     });
-
 }
