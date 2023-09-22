f85f22dc6fa248145523cb43ffca8b50363c6a44
Sylla Ksenia <natoire@yandex.ru>
2023-09-22 12:45:03 +0300

    changed menu

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,14 +1,23 @@
 export function burger() {
     const burgerMenu = document.querySelector(".navbar__burger");
     const burgerList = document.querySelector(".navbar__list");
-
+    const navbarTitle = document.querySelector(".navbar__title");
+    const menuTitle = document.createElement("h2");
     burgerMenu.addEventListener("click", () => {
+        let text = event.target.innerHTML;
         if (burgerList.style.display == "flex") {
             burgerList.style.display = "none";
             burgerMenu.firstElementChild.style.display = "flex";
+            if (text.length > 30) {
+                menuTitle.textContent = "";
+            } else {
+                menuTitle.textContent = text;
+                navbarTitle.insertAdjacentElement("afterbegin", menuTitle);
+            }
         } else {
             burgerList.style.display = "flex";
             burgerMenu.firstElementChild.style.display = "none";
+            menuTitle.remove();
         }
     });
 }
